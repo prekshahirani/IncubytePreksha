c0a26396682bb691447c3454e7a334f1200badde
prekshahirani <prekshahirani19@gmail.com>
2024-07-30 18:53:33 +0530

    Updated add method to handel single digit and added unit test case

--- calc.py
+++ calc.py
@@ -1,11 +1,16 @@
 import unittest
 
 def add(numbers):
-    return 0;
+    if not numbers:
+        return 0
+    return int(numbers)
 
 class TestCalc(unittest.TestCase):
     def test_empty_input(self):
         self.assertEqual(add(""),0)
+    def test_single_input(self):
+        self.assertEqual(add(""), 1)
 
 if __name__ == '__main__':
+    add(2)
     unittest.main()
