f1600f5d43997be3b20ac261c2e29e8a3d50f1e8
prekshahirani <prekshahirani19@gmail.com>
2024-07-30 18:59:14 +0530

    Added unit test case for multiple inputs

--- calc.py
+++ calc.py
@@ -16,6 +16,7 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(add("1"), 1)
     def test_two_input(self):
         self.assertEqual(add("1,2"), 3)
-
+    def test_multiple_input(self):
+        self.assertEqual(add("1,2,3,4"), 10)
 if __name__ == '__main__':
     unittest.main()
