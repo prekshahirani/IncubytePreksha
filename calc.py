fd0ce30050cf4212e0e76cef73a11bb96f135f1b
prekshahirani <prekshahirani19@gmail.com>
2024-08-01 16:51:43 +0530

    Seperated code files of add() and tests.

--- calc.py
+++ calc.py
@@ -22,32 +22,10 @@ def add(numbers):
             sum += int(i)
     return sum
 
-class TestCalc(unittest.TestCase):
-    def test_empty_input(self):
-        self.assertEqual(add(""),0)
-    def test_single_input(self):
-        self.assertEqual(add("1"), 1)
-    def test_two_input(self):
-        self.assertEqual(add("1,2"), 3)
-    def test_multiple_input(self):
-        self.assertEqual(add("1,2,3,4"), 10)
-    def test_new_lines(self):
-        self.assertEqual(add("1\n2,3\n4"), 10)
-    def test_diff_delimiters(self):
-        self.assertEqual(add("//;\n1;2;3;4"), 10)
-    def test_negative_input(self):
-        with self.assertRaises(InvalidNumberException) as cm:
-            add("1,-2,3")
-        self.assertEqual(str(cm.exception), "negative numbers not allowed: -2")
-    def test_all_positive_numbers(self):
-        self.assertEqual(add("1,2,3,4"), 10)
-        self.assertEqual(add("5\n6,7\n8"), 26)
-        self.assertEqual(add("//;\n9;10;11;12"), 42)
-if __name__ == '__main__':
-    print(add("1,2\n3"))
-    print(add("//;\n9;10;11;12"))
-    print(add("//;\n1;2;3;4"))
-    print(add(""))
-    print(add("2"))
-    # print(add("1,-2,-3"))
-    unittest.main()
+print(add("")) #Output : 0
+print(add("2")) #Output : 2
+print(add("2,8")) #Output : 10
+print(add("1,2\n3")) #Output : 6
+print(add("//;\n9;10;11;12")) #Output : 42
+print(add("//;\n1;2;3;4")) #Output : 10
+# print(add("1,-2,-3")) #InvalidNumberException: negative numbers not allowed: -2, -3
---
+++ test_calc.py
@@ -0,0 +1,32 @@
+import unittest
+from calc import add, InvalidNumberException
+
+class TestCalc(unittest.TestCase):
+    def test_empty_input(self):
+        self.assertEqual(add(""),0)
+    def test_single_input(self):
+        self.assertEqual(add("1"), 1)
+    def test_two_input(self):
+        self.assertEqual(add("1,2"), 3)
+    def test_multiple_input(self):
+        self.assertEqual(add("1,2,3,4"), 10)
+    def test_new_lines(self):
+        self.assertEqual(add("1\n2,3\n4"), 10)
+    def test_diff_delimiters(self):
+        self.assertEqual(add("//;\n1;2;3;4"), 10)
+    def test_negative_input(self):
+        with self.assertRaises(InvalidNumberException) as cm:
+            add("1,-2,3")
+        self.assertEqual(str(cm.exception), "negative numbers not allowed: -2")
+    def test_all_positive_numbers(self):
+        self.assertEqual(add("1,2,3,4"), 10)
+        self.assertEqual(add("5\n6,7\n8"), 26)
+        self.assertEqual(add("//;\n9;10;11;12"), 42)
+if __name__ == '__main__':
+    print(add("1,2\n3"))
+    print(add("//;\n9;10;11;12"))
+    print(add("//;\n1;2;3;4"))
+    print(add(""))
+    print(add("2"))
+    # print(add("1,-2,-3"))
+    unittest.main()
